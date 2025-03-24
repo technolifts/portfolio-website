16a7816c8c1058c4aaa25ed618f6d103cc29701a
technolifts <ryan.acarroll@yahoo.com>
2025-03-24 11:04:09 -0700

    Update experience

--- src/content/experience.ts
+++ src/content/experience.ts
@@ -14,16 +14,16 @@ export interface Experience {
   export const experiences: Experience[] = [
     {
       id: 'current-role',
-      title: 'Engineering Program Manager',
+      title: 'Engineering Program Manager / Head of Security GTM',
       company: 'Adobe',
       location: 'Seattle',
-      startDate: '2024',
+      startDate: '2023',
       endDate: 'Present',
       description: [
-        'Automated the process for enrolling engineers in technical security training. Helping 20,000 learners get enrolled in role-based security training in 10% of the time it would take manually.',
-        'Managed Adobe’s Security Champion Program consisting of over 400 champions across all solutions at Adobe to effectively scale security across the company. ',
-        'Established a unified security-developer relations relations by effectively portraying why security matters, and how to impliment security in different engineering teams across Adobe',
-        'Collaborated with the Cloud Security Team to identify high-impact areas, recommend effective policies, and respond to real-time threats.'
+        'Security GTM strategist',
+        'Customer zero for security tools and inititaives',
+        'Head of Adobe Security Champion Program',
+        'Various rotations with DevSecOps team, Red Team and Cloud Security Team'
       ],
       technologies: ['AWS', 'Python', 'Terraform', 'Wiz', 'Databricks', 'GitHub', 'SQL'],
       type: 'security'
@@ -34,11 +34,10 @@ export interface Experience {
       company: 'Adobe',
       location: 'Seattle',
       startDate: '2022',
-      endDate: '2024',
+      endDate: '2023',
       description: [
-        'Created and managed a Security Evangelism program to educate product engineers on top-of-mind Security topics and the Security orgs upcoming initiatives. ',
-        'Developed and deployed two POCs to reduce security specialist response times to security questionaires by 60% and increase deal velocity by 25%. ',
-        'Contributed to the Red Team by operating in Orange Team Campaigns'
+        'Security Evangelism through the business',
+        'Security Developer Relations'
       ],
       technologies: ['Communications', 'Technical Writing'],
       type: 'security'
